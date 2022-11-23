a99cbf7668b7ec30de5178da849b09ddc06cb0a5
Mahjoubi <elyesmah@amazon.com>
2022-11-23 20:04:29 +0200

    flake8 compliant;

--- Letter Combinations of a Phone Number/Recursion.py
+++ Letter Combinations of a Phone Number/Recursion.py
@@ -9,27 +9,27 @@ import collections
 
 class Solution(object):
     mapping = {
-            '2':['a', 'b', 'c'],
-            '3':['d', 'e', 'f'],
-            '4':['g', 'h', 'i'],
-            '5':['j', 'k', 'l'],
-            '6':['m', 'n', 'o'],
-            '7':['p', 'q', 'r', 's'],
-            '8':['t', 'u', 'v'],
-            '9':['w', 'x', 'y', 'z'],
+            '2': ['a', 'b', 'c'],
+            '3': ['d', 'e', 'f'],
+            '4': ['g', 'h', 'i'],
+            '5': ['j', 'k', 'l'],
+            '6': ['m', 'n', 'o'],
+            '7': ['p', 'q', 'r', 's'],
+            '8': ['t', 'u', 'v'],
+            '9': ['w', 'x', 'y', 'z'],
         }
+
     def letterCombinations(self, digits):
         """
         :type digits: str
         :rtype: List[str]
         """
 
-
         def recursion(
-            stack, stack_remaining):
+                stack, stack_remaining):
 
             if not stack_remaining:
-                return stack 
+                return stack
 
             new_stack = collections.deque()
             rem = stack_remaining.popleft()
